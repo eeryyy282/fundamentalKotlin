1df8a3071c8cddd845d0a643efa1bacff2741c3d
eeryyy282 <airiagustus82@gmail.com>
2024-02-25 20:05:45 +0700

    data class and data class alternative

--- src/main/kotlin/dataClasses.kt
+++ src/main/kotlin/dataClasses.kt
@@ -4,11 +4,38 @@ class User(val name: String, val age: Int) {
     }
 }
 
+class  User2(val name: String, val age: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this == other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User2
+
+        if (name != other.name) return false
+        if(age != other.age) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 50 * result + age
+        return result
+    }
+}
+
 data class DataUser(val name: String, val age: Int)
 
 fun main(){
-    val user = User("Juzairi", 20)
+   /* val user = User("Juzairi", 20)
     val dataUser = DataUser("Safitli", 19)
     println(user)
-    println(dataUser)
+    println(dataUser) */
+
+    val dataUser1 = DataUser("Juzairi", 21)
+    val dataUser2 = DataUser("Safitli", 10)
+    val dataUser3 = DataUser("KOtlin", 22)
+
+    println(dataUser2)
+    println(dataUser3)
 }
