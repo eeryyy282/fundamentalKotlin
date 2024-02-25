85c1a4e73de496f859b4c68945d82c8a756a61f3
eeryyy282 <airiagustus82@gmail.com>
2024-02-25 20:55:05 +0700

    enum class or enumeration

---
+++ src/main/kotlin/enumClass.kt
@@ -0,0 +1,20 @@
+// enumclass or enumeration
+
+enum class Color(val value: Int) {
+    RED(0xFF0000),
+    GREEN (0x00FF00),
+    BLUE(0x0000FF)
+}
+
+fun main(){
+    // array
+    val colors: Array<Color> = Color.values()
+    colors.forEach { color ->
+        print("$color\n")
+    }
+
+    //values
+    val newColor: Color = Color.valueOf("RED")
+    println("Color is $newColor")
+    println("Color is ${newColor.value.toString(16)}")
+}
